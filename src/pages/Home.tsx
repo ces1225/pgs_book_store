5f9f4b0518d9bc3719173a767d913d9f4cf1bd92
eunseok <tkdlqfpdlej@naver.com>
2024-11-11 17:40:43 +0900

    2024/11/11 code

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/common/Header";
+import { formatNumber } from "../utils/format";
+
+
+function Home() {
+    return (
+        <>
+        Home Page
+        </>
+    )
+}
+
+export default Home;
